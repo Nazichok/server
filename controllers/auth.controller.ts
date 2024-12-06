47f40e52b5195c18c68125a34ee3d9310c09aa84
Nazar Dizhak <nazar.dizhak@sombrainc.com>
2024-12-06 16:36:40 +0200

    One more try again

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -37,7 +37,7 @@ export const signup = async (req: Request, res: Response) => {
         user.email,
         "Registration Successful",
         { name: user.username, link },
-        "../template/confirmEmail.handlebars"
+        "../../template/confirmEmail.handlebars"
       );
     }
 
@@ -216,7 +216,7 @@ export const resetPasswordRequest = async (req: Request, res: Response) => {
       user.email,
       "Password Reset Request",
       { name: user.username, link: link },
-      "../template/requestResetPassword.handlebars"
+      "../../template/requestResetPassword.handlebars"
     );
 
     return res
@@ -262,7 +262,7 @@ export const resetPassword = async (req: Request, res: Response) => {
       {
         name: user.username,
       },
-      "../template/resetPassword.handlebars"
+      "../../template/resetPassword.handlebars"
     );
 
     return res.status(200).send({ message: "Password reset successfully" });
